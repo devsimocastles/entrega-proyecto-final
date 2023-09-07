6b4268d4125bd73d085add3149345d2ad60376e2
Simon Castillo <simocastles@gmail.com>
2023-09-07 11:11:13 -0300

    añdlkad

--- src/App.js
+++ src/App.js
@@ -8,31 +8,6 @@ import Register from './components/Register';
 import VisualizadorProducto from './components/VisualizadorProducto';
 import NoEncontrado from './components/NoEncontrado';
 
-/*
-  Requisitos de finalización
-Apertura: Sunday, 27 de August de 2023, 00:00
-Cierre: Friday, 8 de September de 2023, 23:59
-
-Consigna:
-
-Desarrollar un ecommerce en react o codeigniter con las siguientes paginas
-
-  Home
-    Mostrar un listado de productos
-  Pagina detalle del producto
-    Mostrar los datos mas destacados del producto
-      Nombre
-      Precio
-      Stock
-      Codigo
-  Registro
-    Formulario de registro con campos a criterio del estudiante
-  Login
-    Email
-    Contraseña
-
-*/
-
 
 function App() {
   return (
